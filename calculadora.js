f7096ade453d3242c14d3512bc35dc820e29a0fe
Luandeson-Mendes <luandeson.mendes@academico.ifpb.edu.br>
2026-09-09 14:57:15 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,11 @@
+// Calculadora em JavaScript
+class operadores {
+
+}
+function calcular(num1, operador, num2){
+    while (true) {
+        if (operador === "+") {
+            return num1 + num2;
+        }
+    }
+}
